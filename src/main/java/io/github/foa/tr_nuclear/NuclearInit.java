98036131833f63d75f8e7ad2ac482376a0f33dd1
Devan-Kerman <dev.sel20@gmail.com>
2021-07-26 22:15:01 -0500

    init

---
+++ src/main/java/io/github/foa/tr_nuclear/NuclearInit.java
@@ -0,0 +1,10 @@
+package io.github.foa.tr_nuclear;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NuclearInit implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/io/github/foa/tr_nuclear/client/NuclearClient.java
@@ -0,0 +1,10 @@
+package io.github.foa.tr_nuclear.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NuclearClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
